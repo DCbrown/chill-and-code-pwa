0365bf45c0de8c1cbbdef8034a85045b5a208c47
dcbrown <browncdonovan@gmail.com>
2021-10-01 15:28:17 -0400

    add scrollable playlist track

--- src/playlist.js
+++ src/playlist.js
@@ -6,62 +6,74 @@ import { faRandom } from '@fortawesome/free-solid-svg-icons'
 
 const songs = [
     { 
+        id: 0,
         name: 'Chambers', 
         artist:"Fat Jon", 
         src: 'https://competent-bhabha-abd8d2.netlify.app/music/chambers.mp3' 
     },
     { 
+        id: 1,
         name: 'Castlevania II Simon\'s Quest', 
         artist:"The One-Ups", 
         src: 'https://competent-bhabha-abd8d2.netlify.app/music/castlevania-II-simon-quest.mp3' 
     },
     { 
+        id: 2,
         name: 'Don\'t Even Try It', 
         artist:"Nujabes", 
         src: 'https://competent-bhabha-abd8d2.netlify.app/music/dont-even-try-it.mp3'
     },
     { 
+        id: 3,
         name: 'Fire Drill', 
         artist:"Gecko Yamori", 
         src: 'https://competent-bhabha-abd8d2.netlify.app/music/fire-drill.mp3'
     },
     { 
+        id: 4,
         name: 'Just Forget', 
         artist:"Force Of Nature", 
         src: 'https://competent-bhabha-abd8d2.netlify.app/music/justforget.mp3'
     },
     { 
+        id: 5,
         name: 'Let Me Know What U Think', 
         artist:"Tsutchie", 
         src: 'https://competent-bhabha-abd8d2.netlify.app/music/let-me-know-what-u-think.mp3'
     },
     { 
+        id: 6,
         name: 'Next To You', 
         artist:"Flamingosis", 
         src: 'https://competent-bhabha-abd8d2.netlify.app/music/next-to-you.mp3'
     },
     { 
+        id: 7,
         name: 'R.A.P Rhythm And PaRapper', 
         artist:"Emcess", 
         src: 'https://competent-bhabha-abd8d2.netlify.app/music/rhythm-and-parappa.mp3'
     },
     { 
+        id: 8,
         name: 'Sincerely', 
         artist:"Tsutchie", 
         src: 'https://competent-bhabha-abd8d2.netlify.app/music/sincerely.mp3'
     },
     { 
+        id: 9,
         name: 'Street Fighter II Sagat Theme', 
         artist:"The One-Ups", 
         src: 'https://competent-bhabha-abd8d2.netlify.app/music/street-fighter-sagat.mp3'
     },
     { 
+        id: 10,
         name: 'ToeJam & Earl Alien Breakdown ', 
         artist:"The One-Ups", 
         src: 'https://competent-bhabha-abd8d2.netlify.app/music/toe-jam-earl-alien-break-down.mp3'
     },
     
 ]
+  
 
 // Ref https://www.npmjs.com/package/react-h5-audio-player
 
@@ -88,6 +100,10 @@ const Playlist = () => {
     const handleToggleShuffle = () => {
         setIsShuffle(isShuffle => !isShuffle)
         // console.log(isShuffle)
+    }
+
+    const handleTrackPick = (id) => {
+        setcurrentMusicIndex(id, songs[id])
     }
 
     return (
@@ -119,6 +135,17 @@ const Playlist = () => {
             {!isShuffle ? <h3>Shuffle Off</h3> : <h3>Shuffle On</h3>}
             
             {/*!isShuffle ? <button onClick={handleRandom}>Shuffle off</button> : <button onClick={handleRandom}>Shuffle on</button>*/}
+
+            <div className="scrollable-list">
+                {songs && songs.map(song => {
+                    return <div 
+                                key={song.id} 
+                                onClick={() => handleTrackPick(song.id)} 
+                                className={currentMusicIndex === song.id ? "list-item-active" : "list-item" }>
+                                    {song.name}
+                            </div>       
+                })}
+            </div> 
 
         </div>
     )
