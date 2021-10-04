3424fc06eac541cbd3fd326cf71db32d9fcb6e1d
dcbrown <browncdonovan@gmail.com>
2021-10-03 22:31:06 -0400

    code refactor

--- src/App.js
+++ src/App.js
@@ -1,13 +1,122 @@
-import Playlist from './playlist';
+import MediaPlayer from './MediaPlayer';
+import React, { useState } from 'react'
 import './App.css';
 
+const songs = [
+  { 
+      id: 0,
+      name: 'Chambers', 
+      artist:"Fat Jon", 
+      src: 'https://competent-bhabha-abd8d2.netlify.app/music/chambers.mp3' 
+  },
+  { 
+      id: 1,
+      name: 'Castlevania II Simon\'s Quest', 
+      artist:"The One-Ups", 
+      src: 'https://competent-bhabha-abd8d2.netlify.app/music/castlevania-II-simon-quest.mp3' 
+  },
+  { 
+      id: 2,
+      name: 'Don\'t Even Try It', 
+      artist:"Nujabes", 
+      src: 'https://competent-bhabha-abd8d2.netlify.app/music/dont-even-try-it.mp3'
+  },
+  { 
+      id: 3,
+      name: 'Fire Drill', 
+      artist:"Gecko Yamori", 
+      src: 'https://competent-bhabha-abd8d2.netlify.app/music/fire-drill.mp3'
+  },
+  { 
+      id: 4,
+      name: 'Just Forget', 
+      artist:"Force Of Nature", 
+      src: 'https://competent-bhabha-abd8d2.netlify.app/music/justforget.mp3'
+  },
+  { 
+      id: 5,
+      name: 'Let Me Know What U Think', 
+      artist:"Tsutchie", 
+      src: 'https://competent-bhabha-abd8d2.netlify.app/music/let-me-know-what-u-think.mp3'
+  },
+  { 
+      id: 6,
+      name: 'Next To You', 
+      artist:"Flamingosis", 
+      src: 'https://competent-bhabha-abd8d2.netlify.app/music/next-to-you.mp3'
+  },
+  { 
+      id: 7,
+      name: 'R.A.P Rhythm And PaRapper', 
+      artist:"Emcess", 
+      src: 'https://competent-bhabha-abd8d2.netlify.app/music/rhythm-and-parappa.mp3'
+  },
+  { 
+      id: 8,
+      name: 'Sincerely', 
+      artist:"Tsutchie", 
+      src: 'https://competent-bhabha-abd8d2.netlify.app/music/sincerely.mp3'
+  },
+  { 
+      id: 9,
+      name: 'Street Fighter II Sagat Theme', 
+      artist:"The One-Ups", 
+      src: 'https://competent-bhabha-abd8d2.netlify.app/music/street-fighter-sagat.mp3'
+  },
+  { 
+      id: 10,
+      name: 'ToeJam & Earl Alien Breakdown ', 
+      artist:"The One-Ups", 
+      src: 'https://competent-bhabha-abd8d2.netlify.app/music/toe-jam-earl-alien-break-down.mp3'
+  },
+  
+]
+
+
 function App() {
+  const [currentMusicIndex, setcurrentMusicIndex] = useState(0);
+  const [isShuffle, setIsShuffle] = useState(false);
+
+  const handleClickPrevious = () => {
+      setcurrentMusicIndex(currentMusicIndex === 0 ? songs.length - 1 : currentMusicIndex - 1)
+      // console.log(currentMusicIndex)
+  }
+
+  const handleClickNext = () => {
+      setcurrentMusicIndex(currentMusicIndex < songs.length - 1 ? currentMusicIndex + 1 : 0)
+      // console.log(currentMusicIndex)
+  }
+
+  const handleRandom = () => {
+      let random = Math.floor(Math.random() * songs.length);
+      // console.log(random)
+      setcurrentMusicIndex(random, songs[random])
+  }
+
+  const handleToggleShuffle = () => {
+      setIsShuffle(isShuffle => !isShuffle)
+      // console.log(isShuffle)
+  }
+
+  const handleTrackPick = (id) => {
+      setcurrentMusicIndex(id, songs[id])
+  }
+
   return (
     <div>
       <div className="container">
         <div className="card">
         <h1 className="header">Chill & Code Playlist</h1>
-          <Playlist></Playlist>
+          <MediaPlayer
+            songs={songs}
+            handleClickNext={handleClickNext}
+            handleClickPrevious={handleClickPrevious}
+            handleRandom={handleRandom}
+            handleToggleShuffle={handleToggleShuffle}
+            handleTrackPick={handleTrackPick}
+            isShuffle={isShuffle}
+            currentMusicIndex={currentMusicIndex}
+            />  
         </div>
       </div>
     </div>
---
+++ src/MediaPlayer.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import AudioPlayer from 'react-h5-audio-player';
+import Playlist from './Playlist';
+import 'react-h5-audio-player/lib/styles.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRandom } from '@fortawesome/free-solid-svg-icons'
+
+
+// Ref https://www.npmjs.com/package/react-h5-audio-player
+
+const MediaPlayer = ({songs, 
+                      currentMusicIndex, 
+                      isShuffle, 
+                      handleClickNext, 
+                      handleRandom, 
+                      handleToggleShuffle, 
+                      handleClickPrevious,
+                      handleTrackPick }) => {
+    return (
+        <div>
+            <h1>{songs[currentMusicIndex].name}</h1>
+            <h2>{songs[currentMusicIndex].artist}</h2>
+            <AudioPlayer
+                className="audioPlayer"
+                autoPlayAfterSrcChange={true}
+                onEnded={!isShuffle ? handleClickNext : handleRandom }
+                loop={false}
+                customAdditionalControls={
+                    [
+                        <FontAwesomeIcon 
+                            icon={faRandom} 
+                            size="2x" 
+                            onClick={handleToggleShuffle}>
+                        </FontAwesomeIcon>
+                    ]
+                }
+                showSkipControls={true}
+                showJumpControls={false}
+                src={songs[currentMusicIndex].src}
+                onClickPrevious={handleClickPrevious}
+                onClickNext={!isShuffle ? handleClickNext : handleRandom }
+            />
+            {/* !isShuffle ? <button onClick={handleShuffle}>Turn On Shuffle</button> : <button onClick={handleShuffle}>Turn Off Shuffle</button> */}
+            
+            {!isShuffle ? <h3>Shuffle Off</h3> : <h3>Shuffle On</h3>}
+            
+            {/*!isShuffle ? <button onClick={handleRandom}>Shuffle off</button> : <button onClick={handleRandom}>Shuffle on</button>*/}
+
+            <Playlist songs={songs} handleTrackPick={handleTrackPick} currentMusicIndex={currentMusicIndex}/>    
+        </div>
+    )
+}
+
+export default MediaPlayer
