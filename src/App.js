cd4bb9f0b1c090dad043e63281ecbb0ef3673afb
dcbrown <browncdonovan@gmail.com>
2021-11-01 23:27:46 -0400

    fix paths

--- src/App.js
+++ src/App.js
@@ -73,13 +73,13 @@ const songs = [
     id: 11,
     name: 'Pretending-to', 
     artist:"Tsutchie", 
-    src: 'https://competent-bhabha-abd8d2.netlify.app/music/pretending-to.mp3'
+    src: 'https://kind-hypatia-a5784c.netlify.app/music/pretending-to.mp3'
   },
   { 
     id: 12,
     name: 'Flight Fantastic', 
     artist:"Flamingosis feat", 
-    src: 'https://competent-bhabha-abd8d2.netlify.app/music/flight-fantastic.mp3'
+    src: 'https://kind-hypatia-a5784c.netlify.app/music/flight-fantastic.mp3'
   },
   
 ]
